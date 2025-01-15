1f07c114e8b639a3e95594943c75347b415a5585
Attiss Ngo <92927591+AttissNgo@users.noreply.github.com>
2025-01-14 20:33:24 -0600

    fix(interchain-token-service): flow limit storage and tests
    - update flow limit storage to avoid redundant Option wrapping
    - add test helpers and improve flow limit tests

--- contracts/interchain-token-service/tests/flow_limit.rs
+++ contracts/interchain-token-service/tests/flow_limit.rs
@@ -1,6 +1,8 @@
 mod utils;
 
-use axelar_gateway::types::Message as GatewayMessage;
+use axelar_gateway::{
+    testutils::TestSignerSet, types::Message as GatewayMessage, AxelarGatewayClient,
+};
 use axelar_soroban_std::{assert_contract_err, assert_invoke_auth_ok, events, traits::BytesExt};
 use interchain_token_service::{
     error::ContractError,
@@ -9,33 +11,75 @@ use interchain_token_service::{
     InterchainTokenServiceClient,
 };
 use soroban_sdk::testutils::{Address as _, Ledger as _};
-use soroban_sdk::{vec, xdr::ToXdr, Address, Bytes, BytesN, Env, String, Vec};
+use soroban_sdk::{vec, xdr::ToXdr, Address, Bytes, BytesN, Env, String};
 use utils::{
     approve_gateway_messages, register_chains, setup_env, setup_gas_token, setup_its_token,
     HUB_CHAIN,
 };
 
-const TEST_FLOW_LIMIT: Option<i128> = Some(1000);
+struct GatewayConfig<'a> {
+    client: AxelarGatewayClient<'a>,
+    signers: TestSignerSet,
+}
+
+struct TokenConfig {
+    id: BytesN<32>,
+    deployer: Address,
+}
+
 const EPOCH_TIME: u64 = 6 * 60 * 60;
 
-fn setup_flow_limit(env: &Env, client: &InterchainTokenServiceClient) -> (BytesN<32>, Address) {
+fn dummy_flow_limit() -> i128 {
+    1000
+}
+
+fn dummy_interchain_transfer_params(env: &Env) -> (String, Bytes, Option<Bytes>) {
+    let destination_chain = String::from_str(&env, "ethereum");
+    let destination_address = Bytes::from_hex(&env, "4F4495243837681061C4743b74B3eEdf548D56A5");
+    let data = None;
+
+    (destination_chain, destination_address, data)
+}
+
+fn setup<'a>() -> (
+    Env,
+    InterchainTokenServiceClient<'a>,
+    GatewayConfig<'a>,
+    TokenConfig,
+) {
+    let (env, client, gateway_client, _, signers) = setup_env();
+
+    register_chains(&env, &client);
+
     let supply = i128::MAX;
-    let deployer = Address::generate(env);
-    let token_id = setup_its_token(env, client, &deployer, supply);
+    let deployer = Address::generate(&env);
+    let token_id = setup_its_token(&env, &client, &deployer, supply);
 
     client
         .mock_all_auths()
-        .set_flow_limit(&token_id, &TEST_FLOW_LIMIT);
+        .set_flow_limit(&token_id, &Some(dummy_flow_limit()));
 
-    (token_id, deployer)
+    (
+        env,
+        client,
+        GatewayConfig {
+            client: gateway_client,
+            signers,
+        },
+        TokenConfig {
+            id: token_id,
+            deployer,
+        },
+    )
 }
 
-fn create_interchain_transfer_message(
+fn execute_its_transfer(
     env: &Env,
     client: &InterchainTokenServiceClient,
+    gateway: &GatewayConfig,
     token_id: &BytesN<32>,
     amount: i128,
-) -> (String, String, String, Bytes, Vec<GatewayMessage>) {
+) {
     let sender = Address::generate(env).to_xdr(env);
     let recipient = Address::generate(env).to_xdr(env);
     let source_chain = client.its_hub_chain_name();
@@ -67,7 +111,9 @@ fn create_interchain_transfer_message(
         },
     ];
 
-    (source_chain, message_id, source_address, payload, messages)
+    approve_gateway_messages(&env, &gateway.client, gateway.signers.clone(), messages);
+
+    client.execute(&source_chain, &message_id, &source_address, &payload);
 }
 
 #[test]
@@ -79,10 +125,10 @@ fn set_flow_limit_succeeds() {
 
     assert_invoke_auth_ok!(
         client.operator(),
-        client.try_set_flow_limit(&token_id, &TEST_FLOW_LIMIT)
+        client.try_set_flow_limit(&token_id, &Some(dummy_flow_limit()))
     );
 
-    assert_eq!(client.flow_limit(&token_id), TEST_FLOW_LIMIT);
+    assert_eq!(client.flow_limit(&token_id), Some(dummy_flow_limit()));
 
     goldie::assert!(events::fmt_last_emitted_event::<FlowLimitSetEvent>(&env));
 }
@@ -92,20 +138,24 @@ fn set_flow_limit_succeeds() {
 fn zero_flow_limit_freezes_token() {
     let (env, client, gateway_client, _, signers) = setup_env();
     register_chains(&env, &client);
-    let (token_id, _) = setup_flow_limit(&env, &client);
+    let gateway = GatewayConfig {
+        client: gateway_client,
+        signers,
+    };
+
+    let supply = i128::MAX;
+    let deployer = Address::generate(&env);
+    let token_id = setup_its_token(&env, &client, &deployer, supply);
 
     client.mock_all_auths().set_flow_limit(&token_id, &Some(0));
 
     let amount = 1;
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, amount);
-    approve_gateway_messages(&env, &gateway_client, signers, messages);
 
-    client.execute(&source_chain, &message_id, &source_address, &payload);
+    execute_its_transfer(&env, &client, &gateway, &token_id, amount);
 }
 
 #[test]
-fn set_flow_limit_fails_invalid_amount() {
+fn set_flow_limit_fails_on_negative_limit() {
     let (env, client, _, _, _) = setup_env();
     let token_id = BytesN::from_array(&env, &[1; 32]);
 
@@ -121,129 +171,105 @@ fn set_flow_limit_fails_invalid_amount() {
 
 #[test]
 fn flow_limit_resets_after_epoch() {
-    let (env, client, gateway_client, _, signers) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, _) = setup_flow_limit(&env, &client);
+    let (env, client, gateway, token) = setup();
 
-    let amount = TEST_FLOW_LIMIT.unwrap();
+    let amount = dummy_flow_limit();
 
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, amount);
-    approve_gateway_messages(&env, &gateway_client, signers.clone(), messages);
-    client.execute(&source_chain, &message_id, &source_address, &payload);
-    assert_eq!(client.flow_in_amount(&token_id), amount);
+    execute_its_transfer(&env, &client, &gateway, &token.id, amount);
+    assert_eq!(client.flow_in_amount(&token.id), amount);
 
-    let current_timestamp = env.ledger().timestamp();
-    env.ledger().set_timestamp(current_timestamp + EPOCH_TIME);
+    env.ledger()
+        .set_timestamp(env.ledger().timestamp() + EPOCH_TIME);
+    assert_eq!(client.flow_in_amount(&token.id), 0);
 
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, amount);
-    approve_gateway_messages(&env, &gateway_client, signers, messages);
-    client.execute(&source_chain, &message_id, &source_address, &payload);
-    assert_eq!(client.flow_in_amount(&token_id), amount);
+    execute_its_transfer(&env, &client, &gateway, &token.id, amount);
+    assert_eq!(client.flow_in_amount(&token.id), amount);
 }
 
 #[test]
 fn add_flow_in_succeeds() {
-    let (env, client, gateway_client, _, signers) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, _) = setup_flow_limit(&env, &client);
+    let (env, client, gateway, token) = setup();
 
-    let amount = TEST_FLOW_LIMIT.unwrap();
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, amount);
-    approve_gateway_messages(&env, &gateway_client, signers, messages);
+    let amount = dummy_flow_limit();
 
-    assert_eq!(client.flow_in_amount(&token_id), 0);
+    assert_eq!(client.flow_in_amount(&token.id), 0);
 
-    client.execute(&source_chain, &message_id, &source_address, &payload);
-
-    assert_eq!(client.flow_in_amount(&token_id), amount);
+    execute_its_transfer(&env, &client, &gateway, &token.id, amount);
+    assert_eq!(client.flow_in_amount(&token.id), amount);
+    assert_eq!(client.flow_out_amount(&token.id), 0);
 }
 
 #[test]
 #[should_panic(expected = "Error(Contract, #20)")] // ContractError::FlowLimitExceeded
 fn add_flow_in_fails_exceeds_flow_limit() {
-    let (env, client, gateway_client, _, signers) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, _) = setup_flow_limit(&env, &client);
+    let (env, client, gateway, token) = setup();
 
-    let amount = TEST_FLOW_LIMIT.unwrap();
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, amount);
-    approve_gateway_messages(&env, &gateway_client, signers.clone(), messages);
+    let amount = dummy_flow_limit();
 
-    client.execute(&source_chain, &message_id, &source_address, &payload);
+    execute_its_transfer(&env, &client, &gateway, &token.id, amount);
+    assert_eq!(client.flow_in_amount(&token.id), amount);
 
     let second_amount = 1;
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, second_amount);
-    approve_gateway_messages(&env, &gateway_client, signers, messages);
 
-    client.execute(&source_chain, &message_id, &source_address, &payload);
+    execute_its_transfer(&env, &client, &gateway, &token.id, second_amount);
 }
 
 #[test]
 fn add_flow_out_succeeds() {
-    let (env, client, _, _, _) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, sender) = setup_flow_limit(&env, &client);
-    let gas_token = setup_gas_token(&env, &sender);
+    let (env, client, _, token) = setup();
+    let gas_token = setup_gas_token(&env, &token.deployer);
 
-    let amount = 1000;
-    let destination_chain = String::from_str(&env, "ethereum");
-    let destination_address = Bytes::from_hex(&env, "4F4495243837681061C4743b74B3eEdf548D56A5");
-    let data = None;
+    let amount = dummy_flow_limit();
+    let (destination_chain, destination_address, data) = dummy_interchain_transfer_params(&env);
 
     client
         .mock_all_auths()
         .set_trusted_chain(&destination_chain);
 
+    assert_eq!(client.flow_out_amount(&token.id), 0);
+
     client.mock_all_auths().interchain_transfer(
-        &sender,
-        &token_id,
+        &token.deployer,
+        &token.id,
         &destination_chain,
         &destination_address,
         &amount,
         &data,
         &gas_token,
     );
-
-    assert_eq!(client.flow_out_amount(&token_id), amount);
+    assert_eq!(client.flow_out_amount(&token.id), amount);
+    assert_eq!(client.flow_in_amount(&token.id), 0);
 }
 
 #[test]
 #[should_panic(expected = "Error(Contract, #20)")] // ContractError::FlowLimitExceeded
 fn add_flow_out_fails_exceeds_flow_limit() {
-    let (env, client, _, _, _) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, sender) = setup_flow_limit(&env, &client);
-    let gas_token = setup_gas_token(&env, &sender);
+    let (env, client, _, token) = setup();
+    let gas_token = setup_gas_token(&env, &token.deployer);
 
-    let amount = TEST_FLOW_LIMIT.unwrap();
-    let destination_chain = String::from_str(&env, "ethereum");
-    let destination_address = Bytes::from_hex(&env, "4F4495243837681061C4743b74B3eEdf548D56A5");
-    let data = None;
+    let amount = dummy_flow_limit();
+    let (destination_chain, destination_address, data) = dummy_interchain_transfer_params(&env);
 
     client
         .mock_all_auths()
         .set_trusted_chain(&destination_chain);
 
     client.mock_all_auths().interchain_transfer(
-        &sender,
-        &token_id,
+        &token.deployer,
+        &token.id,
         &destination_chain,
         &destination_address,
         &amount,
         &data,
         &gas_token,
     );
+    assert_eq!(client.flow_out_amount(&token.id), amount);
 
     let second_amount = 1;
 
     client.mock_all_auths().interchain_transfer(
-        &sender,
-        &token_id,
+        &token.deployer,
+        &token.id,
         &destination_chain,
         &destination_address,
         &second_amount,
@@ -255,36 +281,33 @@ fn add_flow_out_fails_exceeds_flow_limit() {
 #[test]
 #[should_panic(expected = "Error(Contract, #21)")] // ContractError::FlowAmountOverflow
 fn add_flow_fails_on_flow_comparison_overflow() {
-    let (env, client, gateway_client, _, signers) = setup_env();
-    register_chains(&env, &client);
-    let (token_id, sender) = setup_flow_limit(&env, &client);
-    let gas_token = setup_gas_token(&env, &sender);
+    let (env, client, gateway, token) = setup();
+    let gas_token = setup_gas_token(&env, &token.deployer);
+
+    let flow_limit = i128::MAX - 50;
 
     client
         .mock_all_auths()
-        .set_flow_limit(&token_id, &Some(i128::MAX - 50));
+        .set_flow_limit(&token.id, &Some(flow_limit));
 
-    let high_amount = i128::MAX - 100;
-    let (source_chain, message_id, source_address, payload, messages) =
-        create_interchain_transfer_message(&env, &client, &token_id, high_amount);
-    approve_gateway_messages(&env, &gateway_client, signers, messages);
-    client.execute(&source_chain, &message_id, &source_address, &payload);
+    let high_amount = flow_limit - 1;
 
-    let small_amount = 100;
-    let destination_chain = String::from_str(&env, "ethereum");
-    let destination_address = Bytes::from_hex(&env, "4F4495243837681061C4743b74B3eEdf548D56A5");
+    execute_its_transfer(&env, &client, &gateway, &token.id, high_amount);
+
+    let small_amount = 2;
+    let (destination_chain, destination_address, data) = dummy_interchain_transfer_params(&env);
 
     client
         .mock_all_auths()
         .set_trusted_chain(&destination_chain);
 
     client.mock_all_auths().interchain_transfer(
-        &sender,
-        &token_id,
+        &token.deployer,
+        &token.id,
         &destination_chain,
         &destination_address,
         &small_amount,
-        &None,
+        &data,
         &gas_token,
     );
 }
