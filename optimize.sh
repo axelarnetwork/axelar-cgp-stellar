2406a691326861c6f463741fc146bc5c34ba557f
ahramy <ahram@interoplabs.io>
2025-01-22 22:15:53 -0800

    chore: update reusable build upload workflow and optimize script (#195)

--- optimize.sh
+++ optimize.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 release_folder="target/wasm32-unknown-unknown/release"
-prefix="stellar_"
 
 if ! command -v stellar >/dev/null 2>&1; then
     echo "Error: 'stellar' command not found" >&2
@@ -22,18 +21,6 @@ for wasm_file in "$release_folder"/*.wasm; do
             ;;
         *)
             stellar contract optimize --wasm "$wasm_file"
-            ;;
-    esac
-done
-
-# Rename files that start with the prefix and contain "optimized"
-for wasm_file in "$release_folder"/*.wasm; do
-    base_name=$(basename "$wasm_file")
-    case "$base_name" in
-        "${prefix}"*optimized*)
-            # Remove the prefix from the filename
-            new_base_name="${base_name#"${prefix}"}"
-            mv "$wasm_file" "$release_folder/$new_base_name"
             ;;
     esac
 done
