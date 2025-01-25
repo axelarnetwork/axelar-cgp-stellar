9de07178509f1adfd35c7ddfd0c56aa8a474a0f8
ahramy <ahram@interoplabs.io>
2025-01-24 16:06:47 -0800

    update

--- contracts/stellar-axelar-gas-service/src/lib.rs
+++ contracts/stellar-axelar-gas-service/src/lib.rs
@@ -13,9 +13,6 @@ compile_error!("'testutils' feature is not supported on 'wasm' target");
 #[cfg(any(test, feature = "testutils"))]
 pub mod testutils;
 
-#[cfg(test)]
-mod tests;
-
 cfg_if::cfg_if! {
     if #[cfg(all(feature = "library", not(feature = "testutils")))] {
         pub use interface::{AxelarGasServiceClient, AxelarGasServiceInterface};
