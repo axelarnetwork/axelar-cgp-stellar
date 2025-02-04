60dbfb52f981fa3548a06776b2b172756b7bccb6
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2025-02-04 16:40:44 -0500

    address comments

--- packages/stellar-axelar-std-derive/src/storage.rs
+++ packages/stellar-axelar-std-derive/src/storage.rs
@@ -10,6 +10,102 @@ enum Value {
 }
 
 impl Value {
+    /// (constructor) Returns the status xor value type of a storage enum variant.
+    pub fn from_attrs(attrs: &[Attribute]) -> Self {
+        let has_status = attrs.iter().any(|attr| attr.path().is_ident("status"));
+        let value_attrs: Vec<_> = attrs
+            .iter()
+            .filter(|attr| attr.path().is_ident("value"))
+            .collect();
+
+        match (has_status, !value_attrs.is_empty()) {
+            (true, false) => Self::Status,
+            (false, true) => {
+                let attr = value_attrs[0];
+                if let Meta::List(list) = &attr.meta {
+                    Self::Type(
+                        list.parse_args::<Type>()
+                            .expect("failed to parse value type"),
+                    )
+                } else {
+                    panic!("value attribute must contain a type parameter: #[value(Type)]");
+                }
+            }
+            (false, false) => {
+                panic!("missing required attribute: either #[status] xor #[value(Type)]")
+            }
+            (true, true) => {
+                panic!("a storage key cannot have both #[status] and #[value] attributes")
+            }
+        }
+    }
+
+    /// Returns the getter, setter, and remover functions for a storage enum variant.
+    fn fns(&self, enum_name: &Ident, storage_type: &StorageType, variant: &Variant) -> TokenStream {
+        let (getter_name, setter_name, remover_name) = self.fn_names(&variant.ident);
+        let param_list = Self::param_list(variant);
+        let key = Self::key(enum_name, variant);
+
+        let storage_method = storage_type.storage_method();
+        let ttl_fn = storage_type.ttl_method(&key);
+
+        match self {
+            Self::Status => quote! {
+                pub fn #getter_name(#param_list) -> bool {
+                    let value = env.storage()
+                        .#storage_method()
+                        .has(&#key);
+
+                    if value {
+                        #ttl_fn
+                    }
+
+                    value
+                }
+
+                pub fn #setter_name(#param_list) {
+                    env.storage()
+                        .#storage_method()
+                        .set(&#key, &());
+                }
+
+                pub fn #remover_name(#param_list) {
+                    env.storage()
+                        .#storage_method()
+                        .remove(&#key);
+                }
+            },
+            Self::Type(value_type) => quote! {
+                pub fn #getter_name(#param_list) -> Option<#value_type> {
+                    let value = env.storage()
+                        .#storage_method()
+                        .get::<_, #value_type>(&#key);
+
+                    if value.is_some() {
+                        #ttl_fn
+                    }
+
+                    value
+                }
+
+                pub fn #setter_name(#param_list, value: &#value_type) {
+                    env.storage()
+                        .#storage_method()
+                        .set(&#key, value);
+
+                    #ttl_fn
+                }
+
+                pub fn #remover_name(#param_list) {
+                    env.storage()
+                        .#storage_method()
+                        .remove(&#key);
+                }
+            },
+        }
+    }
+
+    /// Returns the getter, setter, and remover names for a storage enum variant.
     fn fn_names(&self, variant_ident: &Ident) -> (Ident, Ident, Ident) {
         let ident = variant_ident.to_string().to_snake_case();
         match self {
@@ -25,11 +121,32 @@ impl Value {
             ),
         }
     }
-}
 
-struct StorageAttributes {
-    storage_type: StorageType,
-    value: Value,
+    /// Returns the parameter list for a storage enum variant.
+    fn param_list(variant: &Variant) -> TokenStream {
+        let (field_names, field_types) = fields_data(&variant.fields);
+
+        if field_names.is_empty() {
+            quote! { env: &soroban_sdk::Env }
+        } else {
+            quote! { env: &soroban_sdk::Env, #(#field_names: #field_types),* }
+        }
+    }
+
+    /// Returns the key for a storage enum variant.
+    fn key(enum_name: &Ident, variant: &Variant) -> TokenStream {
+        let (field_names, _) = fields_data(&variant.fields);
+        let variant_ident = &variant.ident;
+
+        if field_names.is_empty() {
+            quote! { #enum_name::#variant_ident }
+        } else {
+            let field_names = field_names.iter().map(|name| {
+                quote! { #name.clone() }
+            });
+            quote! { #enum_name::#variant_ident(#(#field_names),*) }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -48,10 +165,10 @@ impl StorageType {
         }
     }
 
-    pub fn ttl_method(&self) -> TokenStream {
+    pub fn ttl_method(&self, key: &TokenStream) -> TokenStream {
         match self {
             Self::Persistent => quote! {
-                stellar_axelar_std::ttl::extend_persistent_ttl(env, &key);
+                stellar_axelar_std::ttl::extend_persistent_ttl(env, &#key);
             },
             Self::Instance => quote! {
                 stellar_axelar_std::ttl::extend_instance_ttl(env);
@@ -71,27 +188,21 @@ pub fn contract_storage(input: &DeriveInput) -> TokenStream {
 
     let transformed_variants: Vec<_> = variants.iter().map(transform_variant).collect();
 
-    let public_fns: Vec<_> = variants
+    let fns: Vec<_> = variants
         .iter()
         .map(|variant| {
-            public_storage_fns(
-                name,
-                variant,
-                &StorageAttributes {
-                    storage_type: storage_type(&variant.attrs),
-                    value: value(&variant.attrs),
-                },
-            )
+            Value::from_attrs(&variant.attrs).fns(name, &storage_type(&variant.attrs), variant)
         })
         .collect();
 
     let contract_storage = quote! {
         #[contracttype]
+        #[derive(Clone)]
         enum #name {
             #(#transformed_variants,)*
         }
 
-        #(#public_fns)*
+        #(#fns)*
     };
 
     let contract_storage_tests = contract_storage_tests(name, input);
@@ -159,152 +270,6 @@ fn storage_type(attrs: &[Attribute]) -> StorageType {
     .expect("storage type must be specified exactly once as 'instance', 'persistent', or 'temporary'")
 }
 
-/// Returns the status xor value type of a storage enum variant.
-fn value(attrs: &[Attribute]) -> Value {
-    let has_status = attrs.iter().any(|attr| attr.path().is_ident("status"));
-    let value_attrs: Vec<_> = attrs
-        .iter()
-        .filter(|attr| attr.path().is_ident("value"))
-        .collect();
-
-    match (has_status, !value_attrs.is_empty()) {
-        (true, false) => Value::Status,
-        (false, true) => {
-            let attr = value_attrs[0];
-            if let Meta::List(list) = &attr.meta {
-                Value::Type(
-                    list.parse_args::<Type>()
-                        .expect("failed to parse value type"),
-                )
-            } else {
-                panic!("value attribute must contain a type parameter: #[value(Type)]");
-            }
-        }
-        (false, false) => panic!("missing required attribute: either #[status] xor #[value(Type)]"),
-        _ => panic!("a storage key cannot have both #[status] and #[value] attributes"),
-    }
-}
-
-/// Generates the public module-level storage functions.
-fn public_storage_fns(
-    enum_name: &Ident,
-    variant: &Variant,
-    StorageAttributes {
-        storage_type,
-        value,
-    }: &StorageAttributes,
-) -> TokenStream {
-    let variant_ident = &variant.ident;
-
-    let (field_names, field_types) = fields_data(&variant.fields);
-
-    let storage_method = storage_type.storage_method();
-    let ttl_fn = storage_type.ttl_method();
-
-    let key = if field_names.is_empty() {
-        quote! { #enum_name::#variant_ident }
-    } else {
-        quote! { #enum_name::#variant_ident(#(#field_names),*) }
-    };
-    let param_list = if field_names.is_empty() {
-        quote! { env: &soroban_sdk::Env }
-    } else {
-        quote! { env: &soroban_sdk::Env, #(#field_names: #field_types),* }
-    };
-
-    match &value {
-        Value::Status => value_status_fns(
-            &param_list,
-            &storage_method,
-            key,
-            &ttl_fn,
-            value.fn_names(variant_ident),
-        ),
-        Value::Type(value_type) => value_type_fns(
-            &param_list,
-            &storage_method,
-            key,
-            value_type,
-            &ttl_fn,
-            value.fn_names(variant_ident),
-        ),
-    }
-}
-
-fn value_status_fns(
-    param_list: &TokenStream,
-    storage_method: &TokenStream,
-    key: TokenStream,
-    ttl_fn: &TokenStream,
-    (getter_name, setter_name, remover_name): (Ident, Ident, Ident),
-) -> TokenStream {
-    quote! {
-        pub fn #getter_name(#param_list) -> bool {
-            let value = env.storage()
-                .#storage_method()
-                .has(&#key);
-
-            if value {
-                #ttl_fn
-            }
-
-            value
-        }
-
-        pub fn #setter_name(#param_list) {
-            env.storage()
-                .#storage_method()
-                .set(&#key, &());
-        }
-
-        pub fn #remover_name(#param_list) {
-            env.storage()
-                .#storage_method()
-                .remove(&#key);
-        }
-    }
-}
-
-fn value_type_fns(
-    param_list: &TokenStream,
-    storage_method: &TokenStream,
-    key: TokenStream,
-    value_type: &Type,
-    ttl_fn: &TokenStream,
-    (getter_name, setter_name, remover_name): (Ident, Ident, Ident),
-) -> TokenStream {
-    quote! {
-        pub fn #getter_name(#param_list) -> Option<#value_type> {
-            let key = #key;
-            let value = env.storage()
-                .#storage_method()
-                .get::<_, #value_type>(&key);
-
-            if value.is_some() {
-                #ttl_fn
-            }
-
-            value
-        }
-
-        pub fn #setter_name(#param_list, value: &#value_type) {
-            let key = #key;
-
-            env.storage()
-                .#storage_method()
-                .set(&key, value);
-
-            #ttl_fn
-        }
-
-        pub fn #remover_name(#param_list) {
-            env.storage()
-                .#storage_method()
-                .remove(&#key);
-        }
-    }
-}
-
 /// Returns the field names and types of a storage enum variant.
 fn fields_data(fields: &Fields) -> (Vec<&Option<Ident>>, Vec<&Type>) {
     match fields {
@@ -318,7 +283,8 @@ fn fields_data(fields: &Fields) -> (Vec<&Option<Ident>>, Vec<&Type>) {
     }
 }
 
-fn contract_storage_tests(enum_name: &Ident, input: &DeriveInput) -> TokenStream {
+/// Generates the storage schema tests for a storage enum.
+fn contract_storage_tests(enum_name: &Ident, enum_input: &DeriveInput) -> TokenStream {
     let test_module_name = format_ident!(
         "{}_storage_layout_tests",
         enum_name.to_string().to_snake_case()
@@ -332,34 +298,38 @@ fn contract_storage_tests(enum_name: &Ident, input: &DeriveInput) -> TokenStream
     quote! {
         #[cfg(test)]
         mod #test_module_name {
-            use super::*;
             use prettyplease;
             use quote::quote;
             use syn::DeriveInput;
 
             #[test]
             fn #test_name() {
-                let input = quote! {
-                    #input
+                let r#enum = quote! {
+                    #enum_input
                 };
-                let file: syn::File = syn::parse2(input).unwrap();
-                let formatted = prettyplease::unparse(&file)
-                    .replace("pub fn ", "\npub fn ")
-                    .replace("#[cfg(test)]", "\n#[cfg(test)]");
-                goldie::assert!(formatted);
+                let enum_file: syn::File = syn::parse2(r#enum).unwrap();
+                let formatted_enum = prettyplease::unparse(&enum_file)
+                    .replace("#[instance]", "\n\t#[instance]")
+                    .replace("#[persistent]", "\n\t#[persistent]")
+                    .replace("#[temporary]", "\n\t#[temporary]");
+                goldie::assert!(formatted_enum);
             }
         }
     }
 }
 
+/// Tests the storage schema generation for a storage enum.
 #[cfg(test)]
 mod tests {
+    use syn::{DeriveInput, Fields};
 
-    use super::*;
+    use crate::storage::contract_storage;
+
+    use super::fields_data;
 
     #[test]
     fn storage_schema_generation_succeeds() {
-        let input: DeriveInput = syn::parse_quote! {
+        let enum_input: DeriveInput = syn::parse_quote! {
             enum DataKey {
                 #[instance]
                 #[value(u32)]
@@ -391,12 +361,12 @@ mod tests {
             }
         };
 
-        let generated = contract_storage(&input);
-        let file: syn::File = syn::parse2(generated).unwrap();
-        let formatted = prettyplease::unparse(&file)
+        let storage_module = contract_storage(&enum_input);
+        let storage_module_file: syn::File = syn::parse2(storage_module).unwrap();
+        let formatted_storage_module = prettyplease::unparse(&storage_module_file)
             .replace("pub fn ", "\npub fn ")
             .replace("#[cfg(test)]", "\n#[cfg(test)]");
-        goldie::assert!(formatted);
+        goldie::assert!(formatted_storage_module);
     }
 
     #[test]
