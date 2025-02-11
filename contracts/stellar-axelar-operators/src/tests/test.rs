49a8d40d3ae63e3f3dccab5c243587a3b125baf6
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2025-02-11 18:25:01 -0500

    add test coverage + testutils

--- contracts/stellar-axelar-operators/src/tests/test.rs
+++ contracts/stellar-axelar-operators/src/tests/test.rs
@@ -3,14 +3,14 @@ extern crate std;
 
 use soroban_sdk::testutils::Address as _;
 use soroban_sdk::{
-    contract, contracterror, contractimpl, symbol_short, vec, Address, Env, Symbol, Val, Vec,
+    contract, contracterror, contractimpl, symbol_short, Address, Env, Symbol, Val, Vec,
 };
 use stellar_axelar_std::events::{fmt_last_emitted_event, Event};
 use stellar_axelar_std::{assert_auth, assert_auth_err, assert_contract_err, IntoEvent};
 
 use crate::error::ContractError;
 use crate::event::{OperatorAddedEvent, OperatorRemovedEvent};
-use crate::{AxelarOperators, AxelarOperatorsClient};
+use crate::tests::testutils::{setup_env, TestConfig};
 
 #[contract]
 pub struct TestTarget;
@@ -35,30 +35,6 @@ impl TestTarget {
 
     pub const fn failing_with_error(_env: &Env) -> Result<Val, TestTargetError> {
         Err(TestTargetError::TestError)
-    }
-}
-
-struct TestConfig<'a> {
-    env: Env,
-    owner: Address,
-    client: AxelarOperatorsClient<'a>,
-    target_id: Address,
-}
-
-fn setup_env<'a>() -> TestConfig<'a> {
-    let env = Env::default();
-
-    let owner = Address::generate(&env);
-    let contract_id = env.register(AxelarOperators, (&owner,));
-    let client = AxelarOperatorsClient::new(&env, &contract_id);
-
-    let target_id = env.register(TestTarget, ());
-
-    TestConfig {
-        env,
-        owner,
-        client,
-        target_id,
     }
 }
 
@@ -207,32 +183,5 @@ fn execute_fails_when_target_returns_error() {
             &Symbol::new(&env, "failing"),
             &Vec::<Val>::new(&env),
         )
-    );
-}
-
-const NEW_WASM: &[u8] = include_bytes!("testdata/stellar_axelar_operators.optimized.wasm");
-
-#[test]
-fn migrate_succeeds() {
-    let TestConfig {
-        env, owner, client, ..
-    } = setup_env();
-
-    let new_wasm_hash = env.deployer().upload_contract_wasm(NEW_WASM);
-
-    let operator = Address::generate(&env);
-
-    assert_auth!(owner, client.add_operator(&operator));
-    assert!(client.is_operator(&operator));
-
-    assert_auth!(owner, client.upgrade(&new_wasm_hash));
-
-    let migration_data = vec![&env, operator.clone()];
-
-    assert_auth!(owner, client.migrate(&migration_data));
-
-    assert!(
-        client.is_operator(&operator),
-        "Operator should still exist after migration"
     );
 }
