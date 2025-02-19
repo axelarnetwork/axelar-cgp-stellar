0c3f38fcb6d83681a54db097a644a33e26498c39
ahramy <ahram@interoplabs.io>
2025-02-19 12:38:41 -0800

    updated

--- contracts/stellar-interchain-token-service/src/tests/deploy_remote_canonical_token.rs
+++ contracts/stellar-interchain-token-service/src/tests/deploy_remote_canonical_token.rs
@@ -3,7 +3,6 @@ use soroban_sdk::token::{self, StellarAssetClient};
 use soroban_sdk::{Address, Bytes, BytesN, IntoVal, String, Symbol};
 use soroban_token_sdk::metadata::TokenMetadata;
 use stellar_axelar_gas_service::testutils::setup_gas_token;
-use stellar_axelar_std::types::Token;
 use stellar_axelar_std::{auth_invocation, events};
 
 use super::utils::{setup_env, TokenMetadataExt};
@@ -100,40 +99,6 @@ fn deploy_remote_canonical_token_succeeds() {
             Some(gas_token)
         ),
         gas_service_auth
-    );
-
-    assert_eq!(env.auths(), deploy_remote_canonical_token_auth);
-}
-
-#[test]
-fn deploy_remote_canonical_token_succeeds_without_gas_token() {
-    let (env, client, _, _, _) = setup_env();
-    let spender = Address::generate(&env);
-    let gas_token: Option<Token> = None;
-    let token_address = client.native_token_address();
-    let destination_chain = String::from_str(&env, "ethereum");
-
-    client.register_canonical_token(&token_address);
-
-    client
-        .mock_all_auths()
-        .set_trusted_chain(&destination_chain);
-
-    client.mock_all_auths().deploy_remote_canonical_token(
-        &token_address,
-        &destination_chain,
-        &spender,
-        &gas_token,
-    );
-
-    goldie::assert!(events::fmt_emitted_event_at_idx::<
-        InterchainTokenDeploymentStartedEvent,
-    >(&env, -2));
-
-    let deploy_remote_canonical_token_auth = auth_invocation!(
-        &env,
-        spender,
-        client.deploy_remote_canonical_token(token_address, destination_chain, spender, gas_token)
     );
 
     assert_eq!(env.auths(), deploy_remote_canonical_token_auth);
