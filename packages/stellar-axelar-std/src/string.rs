379fc1c4925f40f2dd5a859c60d7510c237f62cc
TanvirDeol <tanvir@interoplabs.io>
2025-02-27 03:48:28 -0500

    imports and unit tests

--- packages/stellar-axelar-std/src/string.rs
+++ packages/stellar-axelar-std/src/string.rs
@@ -22,3 +22,52 @@ impl StringExt for soroban_sdk::String {
         true
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use soroban_sdk::{Env, String};
+
+    use super::*;
+
+    #[test]
+    fn validate_ascii_strings_are_ascii() {
+        let ascii_strings = [
+            "",
+            "Hello, world!",
+            "The quick brown fox jumps over the lazy dog.",
+            "1234567890",
+            "!@#$%^&*()_+-=[]{}|;:',.<>/?",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "abcdefghijklmnopqrstuvwxyz",
+        ];
+
+        let env = Env::default();
+        for &s in ascii_strings.iter() {
+            let ascii_string = String::from_str(&env, s);
+            assert!(ascii_string.is_ascii());
+        }
+    }
+
+    #[test]
+    fn validate_non_ascii_strings_not_ascii() {
+        let non_ascii_strings = [
+            "Hello, 世界!",
+            "こんにちは世界",
+            "Привет, мир!",
+            "안녕하세요 세계",
+            "مرحبا بالعالم",
+            "Bonjour le monde 🌍",
+            "¡Hola, mundo!",
+            "Γειά σου Κόσμε",
+            "नमस्ते दुनिया",
+            "שלום עולם",
+            "你好，世界",
+        ];
+
+        let env = Env::default();
+        for &s in non_ascii_strings.iter() {
+            let non_ascii_string = String::from_str(&env, s);
+            assert!(!non_ascii_string.is_ascii());
+        }
+    }
+}
